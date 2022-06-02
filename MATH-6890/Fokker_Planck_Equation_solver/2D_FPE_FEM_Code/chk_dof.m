88bd7fe2df377be95b341c491a2aee1e0cfd3c8c
Vignesh Ramakrishnan <97567218+rvignesh10@users.noreply.github.com>
2022-06-02 13:12:41 -0400

    Delete Fokker_Planck_Equation_solver directory

--- MATH-6890/Fokker_Planck_Equation_solver/2D_FPE_FEM_Code/chk_dof.m
+++
@@ -1,9 +0,0 @@
-function bool = chk_dof(Vec,dof)
-% $Author : Vignesh Ramakrishnan$ 
-% $RIN : 662028006$  $Date : November 21, 2021$
-% $Code Version: 1.0$
-% This function returns a boolean which checks if the input dof is present
-% in the input vector array
-
-    bool = any(Vec == dof);
-end
