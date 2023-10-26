7bcbc3ae4eabd2393472f56c3d2ea260a3c10f4e
Vignesh Ramakrishnan <vigneshramakrishnan@crookshanks-29.dynamic.rpi.edu>
2023-10-26 12:21:05 -0400

    adding mpi files

---
+++ MATH-6890/Homework/HW8/parseCommand.h
@@ -0,0 +1,52 @@
+#ifndef PARSE_COMMAND_H
+#define PARSE_COMMAND_H
+
+#include <string>
+using std::string;
+
+// ------------------------------------------------------------------------------------
+// Parse a command from a string "line" and return a integer "value"
+// Example:
+// line= "-Nx=10" (input)
+// command= "-Nx=" (input)
+// value= 10 (output)
+// Return value : 1 if command was found in line
+// 0 if not found
+// ------------------------------------------------------------------------------------
+int parseCommand( const string & line, const string & command, int & value, bool echo) {
+    int len =command.length();
+    if( line.substr(0,len)==command ) {
+        sscanf(line.substr(len).c_str(),"%d",&value);
+	    if (echo)
+            printf("parseCommand: SETTING %s%d\n",command.c_str(),value);
+        return 1;
+    }
+    return 0;
+}
+
+// Parse a double
+int parseCommand( const string & line, const string & command, double & value, bool echo) {
+    int len =command.length();
+    if( line.substr(0,len)==command ) {
+        sscanf(line.substr(len).c_str(),"%le",&value);
+	    if (echo)
+            printf("parseCommand: SETTING %s%e\n",command.c_str(),value);
+        return 1;
+    }
+    return 0;
+}
+
+// Parse a string
+int parseCommand( const string & line, const string & command, string & value, bool echo) {
+    int len =command.length();
+    if( line.substr(0,len)==command ) {
+        value = line.substr(len);
+	    if (echo)
+            printf("parseCommand: SETTING %s%s\n",command.c_str(),value.c_str());
+        return 1;
+    }
+    return 0;
+}
+
+
+#endif
